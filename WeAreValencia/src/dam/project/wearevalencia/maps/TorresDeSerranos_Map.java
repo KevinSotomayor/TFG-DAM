2f4c351de3e7341fa5821638850a909ec428e177
Kevin S. <pelmuss@gmail.com>
2013-05-10 03:00:53 +0200

    Cambios en la pantalla de inicio.
    Aún por decidir si esta será la pantalla de inicio definitiva, de
    momento con pequeños cambios en el layout principal.
    
    Próximamente las clases que presentan los mapas implementarán un
    actionbar en la parte inferior de la pantalla.

--- WeAreValencia/src/dam/project/wearevalencia/maps/TorresDeSerranos_Map.java
+++ WeAreValencia/src/dam/project/wearevalencia/maps/TorresDeSerranos_Map.java
@@ -15,7 +15,6 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import com.actionbarsherlock.app.ActionBar;
