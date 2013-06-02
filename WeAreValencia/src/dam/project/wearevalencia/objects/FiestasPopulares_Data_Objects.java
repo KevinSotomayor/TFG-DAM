2e6a26aac3b36a8c4ee398df1a2cbbaaf0d14417
Kevin S. <pelmuss@gmail.com>
2013-06-02 17:47:49 +0200

    Añadir una fiesta más al apartado de fiestas ..
    populares que es la celebración de San Juan.
    
    Añadir la información de todas y cada una de las fiestas populares en el
    fichero string.
    
    La foto de la celebración de san Juna es cogida del la web: kedin.es.
    y la información de todas las fiestas de turisvalencia.es y la
    información de semana santa de calendario2013.com
    
    NOTA: Intenté pasar los objetos de tipo fiestas populares como
    putSerializable, sin embargo me decía que no podía hacer el cast de ese
    objeto a tipo serializable, así pues implementé de nuevo la interfaz
    parcelable para poder pasar los objetos.

--- WeAreValencia/src/dam/project/wearevalencia/objects/FiestasPopulares_Data_Objects.java
+++ WeAreValencia/src/dam/project/wearevalencia/objects/FiestasPopulares_Data_Objects.java
@@ -12,6 +12,7 @@ public class FiestasPopulares_Data_Objects {
 	private static final int SEMANA_SANTA = 4;
 	private static final int VIRGEN_DESAMPARADOS= 5;
 	private static final int NUEVE_OCTUBRE = 6;
+	private static final int SAN_JUAN = 7;
 	
 	
 	public static ArrayList<FiestasPopulares_Item> obtainFiestasPopulares(Context contexto){
@@ -24,48 +25,57 @@ public class FiestasPopulares_Data_Objects {
 				R.drawable.fiestaspopulares_fallas, 
 				contexto.getString(R.string.fallasCalendar));
 		
+		FiestasPopulares_Item sanJuan = new FiestasPopulares_Item(
+				SAN_JUAN,
+				contexto.getString(R.string.sanJuan), 
+				contexto.getString(R.string.sanJuanContent), 
+				R.drawable.fiestaspopulares_sanjuan, 
+				contexto.getString(R.string.sanJuanCalendar));
+		
 		FiestasPopulares_Item sanVicenteFerrer = new FiestasPopulares_Item(
 				SAN_VICENTE_FERRER,
 				contexto.getString(R.string.sanVicenteFerrer), 
-				contexto.getString(R.string.fallasContent), 
+				contexto.getString(R.string.sanVicenteFerrerContent), 
 				R.drawable.fiestaspopulares_sanvicenteferrer, 
-				contexto.getString(R.string.fallasCalendar));
+				contexto.getString(R.string.sanVicenteFerrerCalendar));
 		
 		FiestasPopulares_Item corpus = new FiestasPopulares_Item(
 				CORPUS,
 				contexto.getString(R.string.corpus), 
-				contexto.getString(R.string.fallasContent), 
+				contexto.getString(R.string.corpusContent), 
 				R.drawable.fiestaspopulares_corpus, 
-				contexto.getString(R.string.fallasCalendar));
+				contexto.getString(R.string.corpusCalendar));
 		
 		FiestasPopulares_Item semanaSanta = new FiestasPopulares_Item(
 				SEMANA_SANTA,
 				contexto.getString(R.string.semanaSanta), 
-				contexto.getString(R.string.fallasContent), 
+				contexto.getString(R.string.semanaSantaContent), 
 				R.drawable.fiestaspopulares_semanasanta, 
-				contexto.getString(R.string.fallasCalendar));
+				contexto.getString(R.string.semanaSantaCalendar));
 		
 		FiestasPopulares_Item vigenDesamparados = new FiestasPopulares_Item(
 				VIRGEN_DESAMPARADOS,
 				contexto.getString(R.string.virgenDesamparados), 
-				contexto.getString(R.string.fallasContent), 
+				contexto.getString(R.string.virgenDesamparadosContent), 
 				R.drawable.fiestaspopulares_virgendesamparados, 
-				contexto.getString(R.string.fallasCalendar));
+				contexto.getString(R.string.virgenDesamparadosCalendar));
 		
 		FiestasPopulares_Item nueveOctubre = new FiestasPopulares_Item(
 				NUEVE_OCTUBRE,
 				contexto.getString(R.string.nueveOctubre), 
-				contexto.getString(R.string.fallasContent), 
+				contexto.getString(R.string.nueveOctubreContent), 
 				R.drawable.fiestaspopulares_nuevedeoctubre, 
-				contexto.getString(R.string.fallasCalendar));
-		
+				contexto.getString(R.string.nueveOctubreCalendar));
+	
 
 		arrayList.add(fallas);
+		arrayList.add(sanJuan);
 		arrayList.add(sanVicenteFerrer);
 		arrayList.add(corpus);
 		arrayList.add(semanaSanta);
 		arrayList.add(vigenDesamparados);
 		arrayList.add(nueveOctubre);
+		
 		
 		return arrayList;
 		
